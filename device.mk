979b531c8e46df7d84f535598a1e5efd7e40becf
henmadx <henma.dix@outlook.com>
2020-04-14 08:09:45 +0000

    capricorn: return fp to MIUI 8.2.8 global
    Change-Id: I5e8f4396395ee8c6a2688ce6007e4c87bd92fc3a

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # FlipFlap
 PRODUCT_PACKAGES += \
